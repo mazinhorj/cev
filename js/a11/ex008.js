e8eef3a1bd123a0f1db4d80cb69280bfe78658c2
Osmar Menezes da Silva <adm.osmarsilva@gmail.com>
2022-10-03 21:17:11 -0300

    js condicoes

---
+++ js/a11/ex008.js
@@ -0,0 +1,11 @@
+console.log('It works!')
+
+var vel = 155.5
+
+console.log(`Sua velocidade foi de ${vel}km/h.`)
+if (vel > 60) {
+    console.log(`Limite ultrapassado. MULTADO!`)
+} else {
+    console.log(`Dentro do limite.`)
+}
+console.log(`Dirija com prudência e atenção!`)
